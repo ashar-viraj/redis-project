f8ec4321527ea1124cfffbd3e8b4b3bbbf5ffaf3
ashar-viraj <virajasharcse@gmail.com>
2026-05-23 23:58:52 +0530

    Reformatting

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,29 @@ using namespace std;
 
 #define MAX_BUFFER_LEN 1024
 
-void send_msg(char buffer[], int client_fd) {
+void send_msg(char buffer[], int client_fd)
+{
   int byte_sent = send(client_fd, buffer, strlen(buffer), 0);
-  
-  if(byte_sent == -1) {
+
+  if (byte_sent == -1)
+  {
     cout << "Error sending the msg.\n";
   }
 }
 
-void recieve_msg(int client_fd, 
-  RESPParser &parser, 
-  RequestHandler &handler,
-  RESPSerializer &serializer
-) {
+void recieve_msg(int client_fd,
+                 RESPParser &parser,
+                 RequestHandler &handler,
+                 RESPSerializer &serializer)
+{
   char buffer[MAX_BUFFER_LEN] = {0};
   const char *pong = "+PONG\r\n";
-  while(true) {
+  while (true)
+  {
     memset(buffer, 0, sizeof(buffer));
 
-    int msg_len = recv(client_fd, buffer, sizeof(buffer)-1, 0);
-    if(msg_len <= 0)
+    int msg_len = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+    if (msg_len <= 0)
       break;
     // cout << "msg len : " << msg_len << (int)buffer[msg_len] << endl;
     buffer[msg_len] = '\0';
@@ -47,9 +50,9 @@ void recieve_msg(int client_fd,
     RESPValue res = handler.handle(req);
 
     string resStr = serializer.serialize(res);
-    
+
     memset(buffer, 0, sizeof(buffer));
-    for(int i = 0; i < resStr.size(); i++)
+    for (int i = 0; i < resStr.size(); i++)
       buffer[i] = resStr[i];
     buffer[resStr.size()] = '\0';
 
@@ -59,41 +62,46 @@ void recieve_msg(int client_fd,
   close(client_fd);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+  if (server_fd < 0)
+  {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (listen(server_fd, connection_backlog) != 0)
+  {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   std::cout << "Waiting for a client to connect...\n";
@@ -109,15 +117,17 @@ int main(int argc, char **argv) {
   RequestHandler handler(store);
   RESPSerializer serializer;
 
-  while(true) {
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if(client_fd < 0) {
+  while (true)
+  {
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    if (client_fd < 0)
+    {
       cout << "Client connection failed.\n";
       continue;
     }
     std::cout << "Client connected\n";
     thread(recieve_msg, client_fd, ref(parser), ref(handler), ref(serializer)).detach();
-  
+
     // recieve_msg(client_fd);
   }
   close(server_fd);
